c6a8b7bb595cb26683d0042ab51b2356c5715542
coolkiid <73600915+coolkiid@users.noreply.github.com>
2025-02-05 18:07:01 +0800

    add handleError

--- packages/artifact/src/internal/shared/tos-client.ts
+++ packages/artifact/src/internal/shared/tos-client.ts
@@ -1,6 +1,22 @@
-import { TosClient } from "@volcengine/tos-sdk";
+import { TosClient, TosClientError, TosServerError } from "@volcengine/tos-sdk"
 import { endpoint, accesskey, secretkey, region } from "../constants"
+import * as core from "@actions/core"
 
+
+export function handleError(error) {
+  if (error instanceof TosClientError) {
+    core.error(`Client Err Msg: ${error.message}`);
+    core.error(`Client Err Stack: ${error.stack}`);
+  } else if (error instanceof TosServerError) {
+    core.error(`Request ID: ${error.requestId}`);
+    core.error(`Response Status Code: ${error.statusCode}`);
+    core.error(`Response Header: ${error.headers}`);
+    core.error(`Response Err Code: ${error.code}`);
+    core.error(`Response Err Msg: ${error.message}`);
+  } else {
+    core.error(`unexpected exception, message: ${error}`);
+  }
+}
 
 export async function createObjectStorageClient(): Promise<TosClient> {
   const opts = endpoint
--- packages/artifact/src/internal/upload/volc-upload-artifact.ts
+++ packages/artifact/src/internal/upload/volc-upload-artifact.ts
@@ -11,8 +11,8 @@ import {
 } from './upload-zip-specification'
 import {getBackendIdsFromToken} from '../shared/util'
 import {createZipUploadStream} from './zip'
-import {FilesNotFoundError, InvalidResponseError} from '../shared/errors'
-import { createObjectStorageClient } from '../shared/tos-client'
+import {FilesNotFoundError} from '../shared/errors'
+import { createObjectStorageClient, handleError } from '../shared/tos-client'
 import { bucketName, repoName } from '../constants'
 
 
@@ -38,31 +38,6 @@ export async function uploadArtifact(
   // get the IDs needed for the artifact creation
   const backendIds = getBackendIdsFromToken()
 
-  // create the artifact client
-  // const artifactClient = internalArtifactTwirpClient()
-
-  // create the artifact
-  // const createArtifactReq: CreateArtifactRequest = {
-  //   workflowRunBackendId: backendIds.workflowRunBackendId,
-  //   workflowJobRunBackendId: backendIds.workflowJobRunBackendId,
-  //   name,
-  //   version: 4
-  // }
-
-  // if there is a retention period, add it to the request
-  // const expiresAt = getExpiration(options?.retentionDays)
-  // if (expiresAt) {
-  //   createArtifactReq.expiresAt = expiresAt
-  // }
-
-  // const createArtifactResp =
-  //   await artifactClient.CreateArtifact(createArtifactReq)
-  // if (!createArtifactResp.ok) {
-  //   throw new InvalidResponseError(
-  //     'CreateArtifact: response from backend was not ok'
-  //   )
-  // }
-
   const client = await createObjectStorageClient()
   const fileName = `${name}-${backendIds.workflowRunBackendId}-${backendIds.workflowJobRunBackendId}.zip`
   const objectName = `artifacts/${repoName}/${fileName}`  
@@ -71,60 +46,26 @@ export async function uploadArtifact(
     zipSpecification,
     options?.compressionLevel
   )
-
-  await client.putObject({
-    bucket: bucketName,
-    key: objectName,
-    body: zipUploadStream
-  })
-
-  // Upload zip to blob storage
-  // const uploadResult = await uploadZipToBlobStorage(
-  //   createArtifactResp.signedUploadUrl,
-  //   zipUploadStream
-  // )
-
-  // finalize the artifact
-  // const finalizeArtifactReq: FinalizeArtifactRequest = {
-  //   workflowRunBackendId: backendIds.workflowRunBackendId,
-  //   workflowJobRunBackendId: backendIds.workflowJobRunBackendId,
-  //   name,
-  //   size: uploadResult.uploadSize ? uploadResult.uploadSize.toString() : '0'
-  // }
-
-  // if (uploadResult.sha256Hash) {
-  //   finalizeArtifactReq.hash = StringValue.create({
-  //     value: `sha256:${uploadResult.sha256Hash}`
-  //   })
-  // }
+  try {
+    await client.putObject({
+      bucket: bucketName,
+      key: objectName,
+      body: zipUploadStream
+    })
+  } catch (error) {
+    handleError(error)
+  }
 
   core.info(`Finalizing artifact upload`)
-
-  // const finalizeArtifactResp =
-  //   await artifactClient.FinalizeArtifact(finalizeArtifactReq)
-  // if (!finalizeArtifactResp.ok) {
-  //   throw new InvalidResponseError(
-  //     'FinalizeArtifact: response from backend was not ok'
-  //   )
-  // }
-
-  // const artifactId = BigInt(finalizeArtifactResp.artifactId)
-  // core.info(
-  //   `Artifact ${name}.zip successfully finalized. Artifact ID ${artifactId}`
-  // )
 
   const { data } = await client.headObject({
     bucket: bucketName,
     key: objectName,
   });
 
-  core.info(
-    `Artifact ${name}.zip successfully finalized. data=${data}`
-  )
-
   return {
     size: Number(data['content-length']),
-    digest: 'xxxx',
+    digest: data['content-md5'],
     id: 0
   }
 }
